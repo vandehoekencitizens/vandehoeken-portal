9fe33ce9d1315f21f7fac588c0cbbf3cdaf2fe78
vandehoekencitizens <elliotscottchan@gmail.com>
2026-02-13 13:48:40 +0800

    Update and rename toast to toast.jsx

--- src/components/UI/toast.jsx
+++ src/components/UI/toast.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+.import * as React from "react";
 import { cva } from "class-variance-authority";
 import { X } from "lucide-react";
 import { cn } from "@/lib/utils";
