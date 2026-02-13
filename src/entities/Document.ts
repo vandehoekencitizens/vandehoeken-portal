e1fd3283cec5db468b58c2f743df88184a611c9f
vandehoekencitizens <elliotscottchan@gmail.com>
2026-02-13 11:20:42 +0800

    Create Document.ts

---
+++ src/entities/Document.ts
@@ -0,0 +1,39 @@
+{
+  "name": "Document",
+  "type": "object",
+  "properties": {
+    "user_email": {
+      "type": "string",
+      "description": "Document owner email"
+    },
+    "document_name": {
+      "type": "string",
+      "description": "Document name"
+    },
+    "document_type": {
+      "type": "string",
+      "enum": [
+        "passport",
+        "id_card",
+        "certificate",
+        "permit",
+        "other"
+      ],
+      "description": "Type of document"
+    },
+    "file_url": {
+      "type": "string",
+      "description": "Document file URL"
+    },
+    "notes": {
+      "type": "string",
+      "description": "Additional notes"
+    }
+  },
+  "required": [
+    "user_email",
+    "document_name",
+    "document_type",
+    "file_url"
+  ]
+}
